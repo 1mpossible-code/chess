9b8a44beef4a04c98d6ce9f7cd4e1e93ea5be18c
qqbeach <danildzekelev@gmail.com>
2023-12-28 21:07:18 +0200

    Create horizontal and vertical axis display.

--- chess/src/components/Board.tsx
+++ chess/src/components/Board.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import './Board.css';
 
+const horizontalAxis = ["A", "B", "C", "D", "E", "F", "G", "H"]
+const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8]
+
 export default function Board() {
-  return <div id="board"></div>
+  let board = [];
+
+  for (let i = 0; i < horizontalAxis.length; i++) {
+    for (let j = 0; j < verticalAxis.length; j ++) {
+      board.push(<div>{horizontalAxis[i]}{verticalAxis[j]}</div>)
+    }
+  }
+  return <div id="board">{board}</div>
 }
 
