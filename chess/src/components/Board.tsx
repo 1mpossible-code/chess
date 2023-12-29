8ccfc19a3f64e3b86bb03a180ed275c87afda53a
qqbeach <danildzekelev@gmail.com>
2023-12-29 13:59:54 +0200

    Render pawn on correct position.

--- chess/src/components/Board.tsx
+++ chess/src/components/Board.tsx
@@ -5,14 +5,32 @@ import './Board.css';
 const horizontalAxis = ["A", "B", "C", "D", "E", "F", "G", "H"]
 const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8]
 
+interface Piece {
+  image: string,
+  x: number,
+  y: number,
+}
+
+let pieces: Piece[] = [];
+
+pieces.push({image: "assets/white-pawn.png", x: 0, y: 1})
+
+
 export default function Board() {
   let board = [];
 
   for (let i = 7; i >= 0; i--) {
     for (let j = 0; j < verticalAxis.length; j++) {
       const number = j + i;
+      let image = undefined;
 
-      board.push(<Tile number={number}></Tile>)
+      pieces.forEach(p => {
+        if (p.x == j && p.y == i) {
+          image = p.image
+        }
+      })
+
+      board.push(<Tile number={number} image={image}></Tile>)
     }
   }
   return <div id="board">{board}</div>
--- chess/src/components/Tile.tsx
+++ chess/src/components/Tile.tsx
@@ -1,13 +1,14 @@
 import './Tile.css';
 
 interface Props {
-  number: number
+  number: number,
+  image?: string,
 }
 
-export default function Tile({number}: Props) {
+export default function Tile({number, image}: Props) {
   if (number % 2 == 0) {
-    return <div className="tile tile-black"><img src="assets/white-pawn.png"/></div>
+    return <div className="tile tile-black"><img src={image}/></div>
   } else {
-    return <div className="tile"></div>
+    return <div className="tile"><img src={image}/></div>
   }
 }
