3b0bc25f2c946f1e7875988f3bed74b0854f02cf
qqbeach <danildzekelev@gmail.com>
2023-12-29 14:38:24 +0200

    Position all pieces on board.

--- chess/src/components/Board.tsx
+++ chess/src/components/Board.tsx
@@ -16,8 +16,27 @@ let pieces: Piece[] = [];
 for(let i = 0; i < 8; i++) {
   pieces.push({image: "assets/white-pawn.png", x: i, y: 1})
 }
+for(let i = 0; i < 8; i++) {
+  pieces.push({image: "assets/black-pawn.png", x: i, y: 6})
+}
 
+pieces.push({image: "assets/black-rook.png", x: 7, y: 7})
+pieces.push({image: "assets/black-rook.png", x: 0, y: 7})
+pieces.push({image: "assets/black-bishop.png", x: 2, y: 7})
+pieces.push({image: "assets/black-bishop.png", x: 5, y: 7})
+pieces.push({image: "assets/black-knight.png", x: 1, y: 7})
+pieces.push({image: "assets/black-knight.png", x: 6, y: 7})
+pieces.push({image: "assets/black-queen.png", x: 3, y: 7})
+pieces.push({image: "assets/black-king.png", x: 4, y: 7})
 
+pieces.push({image: "assets/white-rook.png", x: 7, y: 0})
+pieces.push({image: "assets/white-rook.png", x: 0, y: 0})
+pieces.push({image: "assets/white-bishop.png", x: 2, y: 0})
+pieces.push({image: "assets/white-bishop.png", x: 5, y: 0})
+pieces.push({image: "assets/white-knight.png", x: 1, y: 0})
+pieces.push({image: "assets/white-knight.png", x: 6, y: 0})
+pieces.push({image: "assets/white-queen.png", x: 3, y: 0})
+pieces.push({image: "assets/white-king.png", x: 4, y: 0})
 
 export default function Board() {
   let board = [];
