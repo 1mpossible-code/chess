f8c1e659edc714ec10ad397b7139a8c5cc6bd86e
qqbeach <danildzekelev@gmail.com>
2023-12-31 13:58:13 +0200

    Optimize code for pieces.push

--- chess/src/components/Board.tsx
+++ chess/src/components/Board.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tile from "./Tile";
 import BoardCSS from './Board.module.css';
+import Draggable from 'react-draggable';
 
 const horizontalAxis: string[] = ["A", "B", "C", "D", "E", "F", "G", "H"];
 const verticalAxis: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -13,6 +14,22 @@ interface Piece {
 
 const pieces: Piece[] = [];
 
+for (let p = 0; p < 2; p++){
+  const type = (p == 0) ? "black" : "white";
+  const y = (p === 0 ) ? 7 : 0;
+
+  pieces.push(
+    { image: `assets/${type}-rook.png`, x: 7, y: y },
+    { image: `assets/${type}-rook.png`, x: 0, y: y },
+    { image: `assets/${type}-bishop.png`, x: 2, y: y },
+    { image: `assets/${type}-bishop.png`, x: 5, y: y },
+    { image: `assets/${type}-knight.png`, x: 1, y: y },
+    { image: `assets/${type}-knight.png`, x: 6, y: y },
+    { image: `assets/${type}-queen.png`, x: 3, y: y },
+    { image: `assets/${type}-king.png`, x: 4, y: y },
+  );
+}
+
 for (let i = 0; i < 8; i++) {
   pieces.push({ image: "assets/white-pawn.png", x: i, y: 1 });
 }
@@ -20,31 +37,6 @@ for (let i = 0; i < 8; i++) {
   pieces.push({ image: "assets/black-pawn.png", x: i, y: 6 });
 }
 
-pieces.push(
-  { image: "assets/black-rook.png", x: 7, y: 7 },
-  { image: "assets/black-rook.png", x: 0, y: 7 },
-  { image: "assets/black-bishop.png", x: 2, y: 7 },
-  { image: "assets/black-bishop.png", x: 5, y: 7 },
-  { image: "assets/black-knight.png", x: 1, y: 7 },
-  { image: "assets/black-knight.png", x: 6, y: 7 },
-  { image: "assets/black-queen.png", x: 3, y: 7 },
-  { image: "assets/black-king.png", x: 4, y: 7 },
-  { image: "assets/white-rook.png", x: 7, y: 0 },
-  { image: "assets/white-rook.png", x: 0, y: 0 },
-  { image: "assets/white-bishop.png", x: 2, y: 0 },
-  { image: "assets/white-bishop.png", x: 5, y: 0 },
-  { image: "assets/white-knight.png", x: 1, y: 0 },
-  { image: "assets/white-knight.png", x: 6, y: 0 },
-  { image: "assets/white-queen.png", x: 3, y: 0 },
-  { image: "assets/white-king.png", x: 4, y: 0 }
-);
-
-function grabPiece(e: React.MouseEvent) {
-  const elem = e.target as HTMLElement;
-  if(elem.classList.contains('pieces')) { // DONT WORK
-    console.log(e);
-  }
-}
 
 const Board: React.FC = () => {
   const board: JSX.Element[] = [];
@@ -60,11 +52,11 @@ const Board: React.FC = () => {
         }
       });
 
-      board.push(<Tile key={`${j},${i}`} id={number} image={image} />);
+      board.push(<Tile key={`${j},${i}`} id={number} image={image}/>);
     }
   }
 
-  return <div onMouseDown={e => grabPiece(e)} className={BoardCSS.board}>{board}</div>;
+  return <div className={BoardCSS.board}>{board}</div>;
 };
 
 export default Board;
